cc81c5ac61e547b1dceb6273f54d33470e17e276
megan <meggsnbacon777@gmail.com>
2020-08-20 02:53:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,7 +25,14 @@ def game_hash
    :colors=>["Turquoise","Purple"],
    :players=>{
      "Jeff Adrien"=>
-     {
+     {:number =>0,
+:shoe =>16,
+:points =>22,
+:rebounds =>12,
+:assists =>12,
+:steals =>3,
+:blocks =>1,
+:slam_dunks =>1
        
      }
  }
