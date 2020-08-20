76855c9420a465a0aed60fc8a776bc842892c939
megan <meggsnbacon777@gmail.com>
2020-08-20 02:28:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,17 +2,18 @@ require 'pry'
 def game_hash
   
  "home"=>{
-   team_name:"string",
-   colors:=>["string",]
-   players:=> {:player_name =>"Alan Anderson",
-:number=>,
-:shoe=>,
-:points=>,
-:rebounds=>,
-:assists=>,
-:steals=>,
-:blocks=>,
-:slam_dunks=>}
+   team_name:=>"Brooklyn Nets",
+   colors:=>["Black,White"]
+   players:=> 
+   {:player_name =>"Alan Anderson",
+:number =>0,
+:shoe =>16,
+:points =>22,
+:rebounds =>12,
+:assists =>12,
+:steals =>3,
+:blocks =>1,
+:slam_dunks =>1}
  },
  team_name:"string",
    colors:=>["string",]
