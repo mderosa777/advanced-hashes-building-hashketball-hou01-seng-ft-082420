f1ba25a822ceb3e164f06ca04a0022da6e6f04cd
megan <meggsnbacon777@gmail.com>
2020-08-20 02:24:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,15 +4,15 @@ def game_hash
  "home"=>{
    team_name:"string",
    colors:=>["string",]
-   players:=> {:player_name =>"",
-:number,
-:shoe,
-:points,
-:rebounds,
-:assists,
-:steals,
-:blocks,
-:slam_dunks}
+   players:=> {:player_name =>"Alan Anderson",
+:number=>,
+:shoe=>,
+:points=>,
+:rebounds=>,
+:assists=>,
+:steals=>,
+:blocks=>,
+:slam_dunks=>}
  },
  "away"=>{
    team_name:"string",
