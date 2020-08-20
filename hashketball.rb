168e76d43066142d04f4d9919faa5a04007d6e29
megan <meggsnbacon777@gmail.com>
2020-08-20 05:53:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
    :colors =>["Black,White"],
    :players => [
      {
-:players_name =>"Alan Anderson"=>
+:player_name =>"Alan Anderson",
 :number =>0,
 :shoe =>16,
 :points =>22,
@@ -18,7 +18,7 @@ def game_hash
 :blocks =>1,
 :slam_dunks =>1,
   },{
-:  "Reggie Evans" =>
+:player_name => "Reggie Evans",
 :number =>30,
 :shoe =>14,
 :points =>12,
@@ -28,7 +28,7 @@ def game_hash
 :blocks =>12,
 :slam_dunks =>7,
 },{
-"Brook Lopez"=>
+:player_name =>"Brook Lopez",
 :number =>11,
 :shoe =>17,
 :points =>17,
@@ -37,10 +37,9 @@ def game_hash
 :steals =>3,
 :blocks =>1,
 :slam_dunks =>15 ,
-},
-   "Mason Plumlee"=>
-{
-  :number =>1,
+},{
+:player_name =>"Mason Plumlee",
+:number =>1,
 :shoe =>19,
 :points =>26,
 :rebounds =>11,
@@ -48,10 +47,9 @@ def game_hash
 :steals =>3,
 :blocks =>8,
 :slam_dunks =>5, 
-},
-"Jason Terry"=>
-{
-  :number => 31,
+},{
+:player_name =>"Jason Terry"
+:number => 31,
 :shoe =>15,
 :points =>19,
 :rebounds =>2,
@@ -60,7 +58,7 @@ def game_hash
 :blocks =>11,
 :slam_dunks =>1, 
   } 
- } 
+ ] 
 },
  :away =>
 {
