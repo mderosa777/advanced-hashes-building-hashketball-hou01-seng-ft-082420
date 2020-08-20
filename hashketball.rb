b1d31379c58f8a1722bc2c548aca586545992437
megan <meggsnbacon777@gmail.com>
2020-08-20 02:56:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,26 +17,7 @@ def game_hash
 :blocks =>1,
 :slam_dunks =>1}
  }
- }
-  },
- :away=>
- {
-   :team_name=>"Charlottle Hornets",
-   :colors=>["Turquoise","Purple"],
-   :players=>{
-     "Jeff Adrien"=>
-     {:number =>0,
-:shoe =>16,
-:points =>22,
-:rebounds =>12,
-:assists =>12,
-:steals =>3,
-:blocks =>1,
-:slam_dunks =>1
-       
-     }
- }
-  }},
+ 
      
   
   end                                                                                                                                                                                                   # Write your code here!
