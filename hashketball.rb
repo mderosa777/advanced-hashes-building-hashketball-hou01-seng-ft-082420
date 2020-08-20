468fc89bb5dc474adafb5bacd01e710734498c18
megan <meggsnbacon777@gmail.com>
2020-08-20 03:28:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,6 +80,16 @@ def game_hash
 },
 "Bismack Biyombo"=>
 {
+  :number =>0,
+:shoe =>16,
+:points =>12,
+:rebounds =>4,
+:assists =>7,
+:steals =>22,
+:blocks =>15,
+:slam_dunks =>10, 
+  },
+  "DeSagna Diop"=>
   
 }
     }
