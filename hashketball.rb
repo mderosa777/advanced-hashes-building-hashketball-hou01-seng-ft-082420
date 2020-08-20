b5675fafcecfdb16652aad992f98438fe4b99c76
megan <meggsnbacon777@gmail.com>
2020-08-20 03:02:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,7 +29,15 @@ def game_hash
 :slam_dunks =>7  },
 "Brook Lopez"=>
 {
-
+  :number =>11,
+:shoe =>17,
+:points =>17,
+:rebounds =>19,
+:assists =>10,
+:steals =>3,
+:blocks =>1,
+:slam_dunks =>15 
+},
  }
  
  }
