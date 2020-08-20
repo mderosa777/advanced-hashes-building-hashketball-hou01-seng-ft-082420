a8765414da257efbf1ed99da3c925e54b78159f3
megan <meggsnbacon777@gmail.com>
2020-08-20 04:54:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,7 +69,7 @@ def game_hash
   :team_name =>"Charlotte Hornets",
   :colors =>["Turquoise","Purple"],
   :players =>{
-   :players_name => "Jeff Adrien"
+   :players_name => "Jeff Adrien",
     
       
       :number =>4,
