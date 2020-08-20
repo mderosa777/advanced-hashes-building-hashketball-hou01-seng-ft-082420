e374349a9bf4e173826b8af698b1e73603123ff1
megan <meggsnbacon777@gmail.com>
2020-08-20 03:00:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,15 @@ def game_hash
 :slam_dunks =>1
       },
       "Reggie Evans" =>
-      
+      {
+    :number =>30,
+:shoe =>14,
+:points =>12,
+:rebounds =>12,
+:assists =>12,
+:steals =>12,
+:blocks =>12,
+:slam_dunks =>7  }
 
  }
  
