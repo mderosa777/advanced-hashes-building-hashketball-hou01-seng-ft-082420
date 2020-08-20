f3f8db43313ea08299439ef4beb47c09ec240327
megan <meggsnbacon777@gmail.com>
2020-08-20 02:37:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,9 @@ def game_hash
   {
  "home"=>
  {
-   team_name:=>"Brooklyn Nets",
-   colors:=>["Black,White"],
-   players:=> 
+   :team_name=>"Brooklyn Nets",
+   :colors=>["Black,White"],
+   :players=> 
    {:player_name =>"Alan Anderson",
 :number =>0,
 :shoe =>16,
