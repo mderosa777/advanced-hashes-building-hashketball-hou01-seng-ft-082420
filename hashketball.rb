a95bbc4bfdbc7782cd379faf92bcd51046784d5f
megan <meggsnbacon777@gmail.com>
2020-08-20 00:50:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+def game_hash(home,away)
+  top hash={
+ "home"=>{
+   team_name:"string",
+   colors:"string",
+   players:1,
+ },
+ "away"=>{
+   team_name:"string",
+   colors:"string",
+   players:1,
+ 
+ }}                                                                                                                                                                                                            # Write your code here!
