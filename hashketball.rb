046459185bdb4ffc7920ba443cf77ffe80917bfe
megan <meggsnbacon777@gmail.com>
2020-08-20 02:24:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,17 @@ def game_hash
 :blocks=>,
 :slam_dunks=>}
  },
- 
+ team_name:"string",
+   colors:=>["string",]
+   players:=> {:player_name =>"Alan Anderson",
+:number=>,
+:shoe=>,
+:points=>,
+:rebounds=>,
+:assists=>,
+:steals=>,
+:blocks=>,
+:slam_dunks=>
  "away"=>{
    team_name:"string",
    colors:"string",
