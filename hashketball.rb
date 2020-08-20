a306f8cf16b13fb3831c98634b1d0939a436836a
megan <meggsnbacon777@gmail.com>
2020-08-20 02:18:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,27 +4,27 @@ def game_hash
  "home"=>{
    team_name:"string",
    colors:"string",
-   players:=> [:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks],
+   players:=> [:player_name =>,
+:number,
+:shoe,
+:points,
+:rebounds,
+:assists,
+:steals,
+:blocks,
+:slam_dunks]
  },
  "away"=>{
    team_name:"string",
    colors:"string",
-   players:=>[:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
+   players:=>[:player_name,
+:number,
+:shoe,
+:point,
+:rebounds,
+:assistts,
+:steals,
+:blocks,
 :slam_dunks],
  
  }
