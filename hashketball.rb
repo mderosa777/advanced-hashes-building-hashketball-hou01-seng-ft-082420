8e408978bc7e39bc5eeef830799d9f7b4fd66056
megan <meggsnbacon777@gmail.com>
2020-08-20 06:05:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ def game_hash
   :colors =>["Turquoise","Purple"],
    :players=>[
 {
-:players_name =>"Jeff Adrien",
+:player_name =>"Jeff Adrien",
 :number =>4,
 :shoe => 18,
 :points =>10,
@@ -74,7 +74,7 @@ def game_hash
 :blocks =>7,
 :slam_dunks =>2, 
 },{
-:players_name =>"Bismack Biyombo",
+:player_name =>"Bismack Biyombo",
   :number =>0,
 :shoe =>16,
 :points =>12,
@@ -84,7 +84,7 @@ def game_hash
 :blocks =>15,
 :slam_dunks =>10, 
   },{
-:players_name =>"DeSagna Diop",
+:player_name =>"DeSagna Diop",
 :number =>2,
 :shoe =>14,
 :points =>24,
@@ -94,7 +94,7 @@ def game_hash
 :blocks =>5,
 :slam_dunks =>5, 
 },{
-:players_name =>"Ben Gordan",
+:player_name =>"Ben Gordan",
 :number =>8,
 :shoe =>15,
 :points =>33,
@@ -104,7 +104,7 @@ def game_hash
 :blocks =>1,
 :slam_dunks =>0, 
   },{
-  :players_name =>"Kemba Walker",
+  :player_name =>"Kemba Walker",
 :number =>33,
 :shoe =>15,
 :points =>6,
